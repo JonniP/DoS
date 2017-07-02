6f876d4ab40f692b90519c72b257a78d83607328
Elias Haaralahti <Haaralahti.Elias@gmail.com>
2017-07-02 21:44:51 +0300

    npc cycle script

---
+++ Assets/Scripts/CharacterClass.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CharacterClass : MonoBehaviour {
+    public int age;
+    public string firstName;
+    public string lastName;
+    public string dateOfBirth;
+    public string nationality;
+    public string sex;
+    public string purposeOfStay;
+    public string durationOfStay;
+
+}
