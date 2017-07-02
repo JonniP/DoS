53829e18c10ea07ad9ee0df834de02cbaafd1cf5
Mikael Myyrä <mikael.myyrae@gmail.com>
2017-07-02 20:07:05 +0300

    Added stamping
    StampableSurface and StampingSurface interact to create an image object onto the StampableSurface upon first collision.

---
+++ Assets/Scripts/StampInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StampInfo : MonoBehaviour {
+
+    public bool isApproved;
+    public GameObject stampObject;
+
+    void Start ()
+    {
+        if (stampObject == null) Debug.Log("No object for stamp to spawn");
+    }
+}
---
+++ Assets/Scripts/StampableSurfaceController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StampableSurfaceController : MonoBehaviour {
+
+    private bool _hasBeenStamped = false;
+    public bool HasBeenStamped { get { return _hasBeenStamped; } }
+    private bool _stampValue; //is the stamp approved or denied
+    public bool StampValue { get { return _stampValue; } }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (!_hasBeenStamped)
+        {
+            GameObject other = collider.gameObject;
+            if (other.CompareTag("stamp"))
+            {
+                StampInfo info = other.GetComponent<StampInfo>();
+                if (info != null)
+                {
+                    GameObject stampObj = Instantiate(info.stampObject);
+                    stampObj.transform.SetParent(transform);
+                    //TODO: put this at the right position
+                    stampObj.transform.localPosition = new Vector3();
+                    Vector3 objAngle = stampObj.transform.localRotation.eulerAngles;
+                    stampObj.transform.localRotation = Quaternion.Euler(objAngle.x, 0, objAngle.z);
+
+                    _hasBeenStamped = true;
+                    _stampValue = info.isApproved;
+                }
+                else Debug.Log("Stamp has no info!");
+            }
+        }
+    }
+}
