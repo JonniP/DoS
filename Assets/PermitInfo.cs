7bd016cec1c56594c5b3f76ed34c46215580c0f2
Mikael Myyrä <mikael.myyrae@gmail.com>
2017-07-04 02:44:01 +0300

    Add travel permit with editable info
    Works exactly like the passport.

---
+++ Assets/PermitInfo.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PermitInfo : MonoBehaviour
+{
+
+    public string Name
+    {
+        get { return txtName.text; }
+        set { txtName.text = value; }
+    }
+
+    public string ID
+    {
+        get { return txtID.text; }
+        set { txtID.text = value; }
+    }
+
+    public string PurposeOfStay
+    {
+        get { return txtPurpose.text; }
+        set { txtPurpose.text = value; }
+    }
+
+    public string DurationOfStay
+    {
+        get { return txtDuration.text; }
+        set { txtDuration.text = value; }
+    }
+
+    private TextMesh txtName;
+    private TextMesh txtID;
+    private TextMesh txtPurpose;
+    private TextMesh txtDuration;
+
+    private StampableSurfaceController stampable;
+
+    void Start()
+    {
+        Transform textHolder = transform.Find("TextHolder");
+        if (textHolder == null)
+        {
+            Debug.Log("Permit has no TextHolder!");
+            return;
+        }
+
+        Transform tempObj = textHolder.Find("txt_name");
+        if (tempObj != null) txtName = tempObj.GetComponent<TextMesh>();
+        else Debug.Log("Permit has no txt_name!");
+
+        tempObj = textHolder.Find("txt_id");
+        if (tempObj != null) txtID = tempObj.GetComponent<TextMesh>();
+        else Debug.Log("Permit has no txt_id!");
+
+        tempObj = textHolder.Find("txt_purpose");
+        if (tempObj != null) txtPurpose = tempObj.GetComponent<TextMesh>();
+        else Debug.Log("Permit has no txt_purpose!");
+
+        tempObj = textHolder.Find("txt_duration");
+        if (tempObj != null) txtDuration = tempObj.GetComponent<TextMesh>();
+        else Debug.Log("Permit has no txt_duration!");
+    }
+}
